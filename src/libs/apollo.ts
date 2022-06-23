7f72cd9b9f60da02a57e476f793159bc57c1b60f
Author Name <sergiorbj93@gmail.com>
2022-06-23 09:43:01 -0300

    setting up the project base

---
+++ src/libs/apollo.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qc2kmj3hn601xn1bwyga02/master',
+  cache: new InMemoryCache()
+ })
+
